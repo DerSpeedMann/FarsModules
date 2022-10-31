c72fc3fa5d233ed10e4cbbfe1d6e80a81372c424
Michael Wachlhofer <38493853+DerSpeedMann@users.noreply.github.com>
2022-10-31 18:04:40 +0100

    added sourceAfis tool with gitignore changes

---
+++ SourceAfis/SourceAfis-Tool/SourceAfis-Tool/Program.cs
@@ -0,0 +1,34 @@
+﻿using SourceAFIS;
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+var options = new FingerprintImageOptions { Dpi = 500 };
+FingerprintTemplate candidate, probe;
+
+var imagePath = @"G:\Users\Michaelwa\Desktop\privat\FH\SIM\PRO_02\SourceAfis\SourceAfis-Tool\SourceAfis-Tool\bin\Debug\net6.0\matching.png";
+try
+{
+    probe = new FingerprintTemplate(
+        new FingerprintImage(File.ReadAllBytes(imagePath))
+    );
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Could not load Image! ({e.GetType()})");
+    return 1;
+}
+try
+{
+    candidate = new FingerprintTemplate(
+        new FingerprintImage(320, 407, File.ReadAllBytes("candidate.dat"), options)
+    );
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Could not load Template! ({e.GetType()})");
+    return 2;
+}
+double score = new FingerprintMatcher(probe).Match(candidate);
+Console.WriteLine("score");
+
+return 0;
