ee27b87a75badeb6a674bd0066ce76c49bbec445
Michael Wachlhofer <38493853+DerSpeedMann@users.noreply.github.com>
2023-01-09 05:49:17 +0100

    fixed maven dependencies and visualization integration

--- SourceAfis/SourceAfis-java-Tool/src/main/java/Main.java
+++ SourceAfis/SourceAfis-java-Tool/src/main/java/Main.java
@@ -1,12 +1,23 @@
 import com.machinezoo.fingerprintio.TemplateFormat;
 import com.machinezoo.sourceafis.FingerprintCompatibility;
+import com.machinezoo.sourceafis.FingerprintImage;
 import com.machinezoo.sourceafis.FingerprintTemplate;
+import com.machinezoo.sourceafis.transparency.TransparencyArchive;
+import com.machinezoo.sourceafis.transparency.TransparencyBuffer;
+import com.machinezoo.sourceafis.transparency.TransparencyKey;
+import com.machinezoo.sourceafis.visualization.*;
+import com.machinezoo.sourceafis.visualization.keys.ShuffledMinutiaeVisualizer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+
     static FingerprintTemplate decoded;
     public static void main(String[] args) {
 
@@ -20,16 +31,40 @@ public class Main {
             case "-c":
                 result = importTemplate(args[2]);
 
-                if(result != 0) System.exit(result);
+                if(result != 0) break;
 
                 result = exportTemplate(args[3], args[1], decoded);
+                break;
+            case "-v":
+                var buffer = new TransparencyBuffer()
+                        .capture(() -> importTemplate(args[2]))
+                        .toArchive();
+                result = visualize(buffer);
                 break;
             default:
                 printUsageInfo();
         }
-
-
         System.exit(result);
+    }
+    private static int visualize(TransparencyArchive archive){
+        var visualizer = new ShuffledMinutiaeVisualizer();
+        var image = visualizer.visualize(archive);
+        createSvgFile(image.tree().toString(), "image.svg");
+        return 0;
+    }
+    private static int createSvgFile(String svgText, String filePath)
+    {
+        try{
+            File file = new File(filePath);
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(svgText);
+            bufferedWriter.close();
+        }catch (IOException e){
+            System.out.println("Error 3: Could not create SVG Image" + filePath);
+            return 4;
+        }
+        return 0;
     }
     private static int importTemplate(String in_template){
         try {
@@ -95,11 +130,11 @@ public class Main {
                         "<out_tmp.*> is the fingerprint template to convert to\n" +
                         "supported formats:\n" +
                                 "SourceAFIS\n" +
-                                "ANSI_378_2004,\n" +
-                                "ANSI_378_2009,\n" +
-                                "ANSI_378_2009_AM1,\n" +
-                                "ISO_19794_2_2005,\n" +
-                                "ISO_19794_2_2011;");
+                                "ANSI_378_2004\n" +
+                                "ANSI_378_2009\n" +
+                                "ANSI_378_2009_AM1\n" +
+                                "ISO_19794_2_2005\n" +
+                                "ISO_19794_2_2011");
     }
 
     static String getJarName(){
